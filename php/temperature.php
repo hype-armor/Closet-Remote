c855c77894d3075261652b438352e8629e62f0c9
Greg <greg@Gregs-MacBook-Pro.local>
2016-10-22 01:25:55 -0500

    Update temperature display.

--- php/temperature.php
+++ php/temperature.php
@@ -1,6 +1,13 @@
 <?php
-$output = exec('cat /sys/bus/w1/devices/28-0000079e4172/w1_slave');
-$temp = (explode("=",$output));
-$floatTemp = floatval($temp[1]);
-echo round($floatTemp / 1000, 2);
+$cpuTempSensor = exec('cat /sys/class/thermal/thermal_zone0/temp');
+$airTempSensor = exec('cat /sys/bus/w1/devices/28-0000079e4172/w1_slave');
+
+$cpuTemp = round(floatval($cpuTempSensor) / 1000, 0);
+
+$airTemp = round(floatval(explode("=",$airTempSensor)[1]) / 1000, 0);
+
+echo 'CPU: ';
+echo $cpuTemp;
+echo ' Air: ';
+echo $airTemp;
 ?>
