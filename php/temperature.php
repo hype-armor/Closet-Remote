574c0ee35e8f669d62ac4ffadb04bb485fdfd924
Greg <greg@Gregs-MacBook-Pro.local>
2016-10-23 11:36:16 -0500

    update pi system usage info.

--- php/temperature.php
+++ php/temperature.php
@@ -1,4 +1,8 @@
 <?php
+$cpuUsage = exec('top -bn1 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk \'{print 100 - $1"%"}\'');
+$memUsage = round(floatval(exec('top -bn1 | awk \'FNR == 4 {print $3/($3+$5)*100}\'')),0);
+
 $cpuTempSensor = exec('cat /sys/class/thermal/thermal_zone0/temp');
 $airTempSensor = exec('cat /sys/bus/w1/devices/28-0000079e4172/w1_slave');
 
@@ -7,7 +11,14 @@ $cpuTemp = round(floatval($cpuTempSensor) / 1000, 0);
 $airTemp = round(floatval(explode("=",$airTempSensor)[1]) / 1000, 0);
 
 echo 'CPU: ';
+echo $cpuUsage;
+echo ' ';
+echo 'MEM: ';
+echo $memUsage;
+echo '% ';
 echo $cpuTemp;
+echo '°C ';
 echo ' Air: ';
 echo $airTemp;
+echo '°C';
 ?>
