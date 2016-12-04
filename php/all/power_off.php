8968a9b02e83cb46537a13fd0eed47b517eead48
Greg <greg@Gregs-MacBook-Pro.local>
2016-12-04 15:34:58 -0600

    add projector power off and tweak apple tv power off.

--- php/all/power_off.php
+++ php/all/power_off.php
@@ -4,8 +4,13 @@
 	// attempt to turn on hdmi switch.
 	system ("irsend SEND_ONCE hdmi POWER");
 
+	// turn off projector 
+	system ("irsend SEND_ONCE viewsonic KEY_POWER");
+	sleep(2);
+	system ("irsend SEND_ONCE viewsonic KEY_POWER");
+
 	// turn off Apple TV
 	system ("irsend SEND_START appletv KEY_PLAY");
-    sleep(5);
+    sleep(10);
 	system ("irsend SEND_STOP appletv KEY_PLAY");
 ?>
